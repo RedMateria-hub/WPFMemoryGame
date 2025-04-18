c5a859fc4b4ff9a4dd66f15318b4275c7c9e03fd
Enache Nicolae <116025204+RedMateria-hub@users.noreply.github.com>
2025-04-18 23:48:23 +0300

    Add files via upload

---
+++ Services/UserStorageService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using MemoryGame.Models;
+
+namespace MemoryGame.Services
+{
+    public static class UserStorageService
+    {
+        private static readonly string FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "users.json");
+
+        public static void SaveUsers(List<User> users)
+        {
+            var json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePath, json);
+        }
+
+        public static List<User> LoadUsers()
+        {
+            if (!File.Exists(FilePath))
+                return new List<User>();
+
+            var json = File.ReadAllText(FilePath);
+            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        }
+    }
+}
---
+++ Views/ValidateUser.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using MemoryGame.Models;
+using MemoryGame.ViewModels;
+
+namespace MemoryGame
+{
+    public partial class ValidateUser : Window
+    {
+        public User NewUser => ((ValidateUserViewModel)DataContext).NewUser;
+
+        public ValidateUser(ObservableCollection<User> users, User user = null)
+        {
+            InitializeComponent();
+
+            var vm = new ValidateUserViewModel(users, user);
+            vm.CloseAction = Close;
+            DataContext = vm;
+        }
+
+        private void PrevImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ValidateUserViewModel vm)
+                vm.PreviousPicture();
+        }
+
+        private void NextImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ValidateUserViewModel vm)
+                vm.NextPicture();
+        }
+
+        private void Close(bool? result)
+        {
+            DialogResult = result;
+            Close();
+        }
+
+        public bool UserValidated()
+        {
+            if (DataContext is ValidateUserViewModel vm)
+            {
+                return vm.IsValidated;
+            }
+            return false;
+        }
+    }
+}
